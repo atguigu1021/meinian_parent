0722e3a577f8577fa5e03652cbd80763dda08697
ChangZihui <a3155385960@outlook.com>
2021-02-02 15:06:07 +0800

    第二次修改无用的工具类，冲突问题

--- meinian_common/src/main/java/com/atguigu/util/UselessClass.java
+++ meinian_common/src/main/java/com/atguigu/util/UselessClass.java
@@ -2,7 +2,8 @@ package com.atguigu.util;
 
 public class UselessClass {
     public static void main(String[] args) {
-        System.out.println("1111111");
+        System.out.println("1111111master");
         System.out.println("2222222");
+
     }
 }
