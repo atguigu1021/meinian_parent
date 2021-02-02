ee1a4c681df17f631fb6317f1c4e4b72eb6c131b
ChangZihui <a3155385960@outlook.com>
2021-02-02 21:07:32 +0800

    修改代码，测试PullRequest

--- meinian_common/src/main/java/com/atguigu/util/UselessClass.java
+++ meinian_common/src/main/java/com/atguigu/util/UselessClass.java
@@ -4,7 +4,7 @@ public class UselessClass {
     public static void main(String[] args) {
         System.out.println("1111111master");
         System.out.println("2222222");
-
+        System.out.println("修改了代码，用于Pull Request");
         System.out.println("1111111");
         System.out.println("2222222dev");
     }
