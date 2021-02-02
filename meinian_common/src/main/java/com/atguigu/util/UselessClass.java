20c3fc26c0e5487f80a46d3a90480e7070b08a1f
ChangZihui <a3155385960@outlook.com>
2021-02-02 15:07:09 +0800

    dev修改无用的工具类，冲突问题

--- meinian_common/src/main/java/com/atguigu/util/UselessClass.java
+++ meinian_common/src/main/java/com/atguigu/util/UselessClass.java
@@ -3,6 +3,6 @@ package com.atguigu.util;
 public class UselessClass {
     public static void main(String[] args) {
         System.out.println("1111111");
-        System.out.println("2222222");
+        System.out.println("2222222dev");
     }
 }
